d4447805476373ac501a6cb1639b617eece6794d
Andrey Marques <andreymarques958@gmail.com>
2024-03-22 01:04:03 -0500

    first commit

---
+++ validacao.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById('submit');
+const form = document.getElementsByClassName('formulario');
+const inputs = document.querySelectorAll('.inputs');
+
+submit.addEventListener('submit', Element=>{
+    const campo = inputs.forEach.length===0;
+    console.log(campo);
+});
+
