9405743d598ff2fe6d12d377a0b3d275582dc6ac
Andrey Marques <andreymarques958@gmail.com>
2024-03-27 14:50:07 -0500

    correção de erro

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
 const inputs = document.querySelectorAll('.inputs');
-const spans = document.getElementsByTagName('span');
 const submit = document.getElementById('submit');
 
-//função para inputs invalidos   
-function erro (index){
-    inputs[index].classList.add('invalid');
-    spans[index].classList.add('span-invalid');
-}
-//função para inputs validos
-function checked(index){
-    spans[index].style.display = 'none'
-}
-//input name
-function nameCheck(){
-    if(inputs[0].value.length < 3 ){
-        erro(0);
-    }
-    else{
-        inputs[0].classList.add('checked');
-        checked(0);
-    }
-}
+submit.addEventListener('click', Element=>{
+    Element.preventDefault()
 
-//input email
-function emailCheck(){
-    if(inputs[1].value.length < 3 ){
-        erro(1);
-    }
-    else{
-        inputs[1].classList.add('checked');
-        checked(1);
-    }
-}
-
-//input tel
-function telCheck(){
-    if(inputs[2].value.length < 3 ){
-        erro(2);
-    }
-    else{
-        inputs[2].classList.add('checked');
-        checked(2);
-    }
-}
-
-//input mensagem/textarea
-function msgCheck(){
-    if(inputs[3].value.length < 3 ){
-        erro(3);
-    }
-    else{
-        inputs[3].classList.add('checked');
-        checked(3);
-    }
-}
-
-
-
- 
+    inputs.forEach(input=>{
+        if(input.value){
+           input.classList.add('checked');  
+        }
+        else{
+            input.classList.add('invalid');
+            input.nextElementSibling.classList.add('span-invalid');
+        }
+    });
+});
